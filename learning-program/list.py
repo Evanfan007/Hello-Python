580800294e974a09c89aec20a0e163b74afbb0e9
Fan Aofeng <fanaofeng@outlook.com>
2021-11-01 22:51:59 +0800

    2021.11.1 previous program
    Signed-off-by: Fan Aofeng <fanaofeng@outlook.com>

---
+++ learning-program/list.py
@@ -0,0 +1,5 @@
+list = [ 'abcd', 786 , 2.23, 'runoob', 70.2 ]
+tinylist = [123, 'runoob']
+rlist = list[-1:-4:-1]
+print(rlist)
+#可以理解为包含起点切片点，不包含终点切片点
---
+++ learning-program/type.py
@@ -0,0 +1,4 @@
+a = 1
+b = 2
+c = "hello world"
+print (type(a), type(b),type(c))
